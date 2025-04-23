9fa56986e9a447d68da39bf89eb4c7a0b96a4e03
Théo Guyonneau <theo.guyonneau@egis-group.com>
2025-04-23 10:14:58 +0200

    Ajout du bloc RECEIVERS
    - Ajout du bloc RECEIVERS
    
    - Mise en forme du code

--- PARAM_GEN/Class.py
+++ PARAM_GEN/Class.py
@@ -30,6 +30,14 @@ class Input:
 
     def set_materials(self, mat_list):
         self.mat_list = mat_list
+
+    def set_receivers(self, file, isamp, field, AtNode, extra):
+        self.rec_dic = {}
+        self.rec_dic['file'] = file
+        self.rec_dic['isamp'] = isamp
+        self.rec_dic['field'] = field
+        self.rec_dic['AtNode'] = AtNode
+        self.rec_dic['extra'] = extra
     
     def set_snapshot(self, itd, fields, components, ps=False, bin=False):
         self.snap_dic = {}
@@ -38,12 +46,14 @@ class Input:
         self.snap_dic['components'] = components
         self.snap_dic['ps'] = ps
         self.snap_dic['bin'] = bin
+    
 
     def write_file(self):
         self.write_general_parameters()
         self.write_mesh()
         self.write_materials()
         self.write_time_scheme()
+        self.write_receivers()
         self.write_snapshot()
 
     def write_from_dictionnary(self, target_dic):
@@ -130,6 +140,15 @@ class Input:
         input_file.close()
         self.write_from_dictionnary(self.time_scheme_dic)
 
+    def write_receivers(self):
+        input_file = open(self.path, 'a')
+        input_file.write("\n")
+        input_file.write("\n #---------- Receivers ----------")
+        input_file.write("\n")
+        input_file.write("\n&REC_LINE")
+        input_file.close()
+        self.write_from_dictionnary(self.rec_dic)
+
     def write_snapshot(self):
         input_file = open(self.path, 'a')
         input_file.write("\n")
@@ -178,24 +197,3 @@ class Mesh():
             self.mesh_dic[key] = val
 
 
-
-Test = Input('./PARAM_GEN/Par.inp')
-Test.set_general_parameters(ndof=2, ItInfo=1000, verbose='1101')
-Test.set_time_scheme(TotalTime=10, dt=1e-5, courant=0.3)
-
-
-Mat = Material(1, "IWAN")
-Mat.set_properties(rho=1000., cp=100., cs=200., Nspr=50, gref=0.000365)
-
-Mat2 = Material(2, 'ELAST')    
-Mat2.set_properties(rho=1., cp=2., cs=3.)
-
-mesh = Mesh('LAYERED')
-mesh.set_properties(xlim=(0.,5.), zmin=-20., nx=1, file="layers")
-
-Test.set_snapshot(1000, 'V', 'xz', False, False)
-
-Test.set_mesh(mesh)
-Test.set_materials([Mat,Mat2])
-
-Test.write_file()
